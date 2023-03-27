a944188b1cc06786d1e17ebe61b377b221eaae68
louis-marceron <louis.marceron@protonmail.com>
2023-03-27 14:40:15 +0200

    refactor: put each error handling middleware in its own file

--- src/app.js
+++ src/app.js
@@ -2,40 +2,31 @@
 const express = require('express');
 const helmet = require('helmet');
 
+// Import error handling middleware
+const notFoundErrorHandler = require('./middlewares/error-handlers/not-found-error-handler.js');
+const genericErrorHandler = require('./middlewares/error-handlers/generic-error-handler.js');
+
 // Import custom routes
 // const exampleRoutes = require('./routes/example');
 
 // Create an Express application
 const app = express();
 
-// Middleware (function that runs before the request handler)
+// Express or module-specific middlewares
 app.use(helmet()); // Add security-related HTTP headers to the application 
 // Explanation for express.json and urlencoded : https://stackoverflow.com/a/51844327
 app.use(express.json()); // Allow to recognize the incoming request object as a JSON object
 app.use(express.urlencoded({ extended: true })); // Allow to recognize the incoming request object as strings or arrays
 
+// Custom middlewares
+// app.use(customMiddleware);
+
+// Error handling middlewares (must be added after all other middlewares)
+app.use(notFoundErrorHandler);
+app.use(genericErrorHandler);
+
 // Use custom routes in the application
 // app.use('/example', exampleRoutes);
 
-// Catch 404 errors and forward them to the error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  // Set the response status to the error status, or default to 500 (Internal Server Error)
-  res.status(err.status || 500);
-
-  // Send the error message as a response
-  res.json({
-    message: err.message,
-    // Only send the error details in development mode to prevent leaking sensitive information
-    error: process.env.NODE_ENV === 'development' ? err : {},
-  });
-});
-
-// Export the app module for use in other files (e.g., index.js)
+// Export the app module for use in other files (index.js)
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Import the required dependencies
 const http = require('http');
-const app = require('./app');
+const app = require('./app'); // Import the app module from app.js
 
 // Load the environment variables from the .env file
 require('dotenv').config();
@@ -8,6 +8,7 @@ require('dotenv').config();
 // Set the port on which the server will listen for incoming requests
 const port = process.env.PORT || 3000;
 
+// Pass the app object as the request listener (a function that is called whenever the server receives a request)
 const server = http.createServer(app);
 
 // Start the server and listen for incoming requests on the specified port
