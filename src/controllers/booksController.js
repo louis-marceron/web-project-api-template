afa8216f99d42383affbd171f22eaa6be1fed804
louis-marceron <louis.marceron@protonmail.com>
2023-03-30 19:13:19 +0200

    doc: remove useless FIXME comments

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -37,11 +37,9 @@ exports.updateBook = async (req, res) => {
             res.status(200).json({ message: 'Book updated successfully' });
         } else {
             res.status(404).json({ message: 'Book not found' });
-            // FIXME redirect to the generic error handler
         }
     } catch (error) {
         res.status(400).json({ message: 'Error updating book', error });
-        // FIXME redirect to the generic error handler
     }
 };
 
