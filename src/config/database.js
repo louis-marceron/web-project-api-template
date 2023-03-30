68a042c3b2cdd72b906ef8792254af2b00d66aa8
louis-marceron <louis.marceron@protonmail.com>
2023-03-30 15:21:41 +0200

    refactor: simplify the sequelize connection request It uses only one environment variable now

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,5 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOSTNAME,
-    dialect: process.env.DB_DIALECT,
-});
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 module.exports = sequelize;
